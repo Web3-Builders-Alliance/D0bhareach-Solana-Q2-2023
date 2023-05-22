22eb164523ae8e2710653095d68991eb7388d596
d0bhareach <viacheslav.modestov@gmail.com>
2023-05-22 15:44:57 +0200

    NOT WORKING! Left it for demonstration.

--- cluster2/wba-vault/programs/wba-vault/src/lib.rs
+++ cluster2/wba-vault/programs/wba-vault/src/lib.rs
@@ -1,15 +1,68 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("HAXGA1FwMfL1pgdAeexVTADUbJ6Fk3ziTWKTD9jyjfbt");
+
+// need to initialaze vault to store 
 
 #[program]
 pub mod wba_vault {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let vault = &mut ctx.accounts.vault;
+        vault.score =  0;
+        vault.auth_bump = *ctx.bumps.get("auth").unwrap();
+        vault.vault_bump = *ctx.bumps.get("vault_holder").unwrap();
+        vault.owner = *ctx.accounts.owner.key;
+        Ok(())
+    }
+    
+    pub fn update_score(ctx: Context<VaultHolder>) -> Result<()> {
+        let vault = &mut ctx.accounts.vault;
+        vault.score += 1;
         Ok(())
     }
 }
-
+// vault must be owned by who? I think the program itself. It's done automatically according to dosc.
+// this struct is for shape of context passed to initialized function.
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    /// CHECK:  this account is only to authorize vault_holder owned by my wallet.
+    #[account(seeds=[b"auth", owner.key().as_ref()], bump)]
+    pub vault_auth: UncheckedAccount<'info>,
+
+    /// CHECK: this account is more down the line
+    #[account(seeds=[b"vault_holder", vault_auth.key().as_ref()], bump)]
+    pub vault_holder: UncheckedAccount<'info>,
+
+    // account to hold vault data, seeded from vault_holder
+    #[account(init, payer = owner,
+        space = 8 + crate::Vault::VAULT_DATA_SIZE)]
+    pub vault: Account<'info, Vault>,
+    // account to pay, In my case it's my wallet. It used to create PDA which 
+    // is used to hold status of program in the vault.
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// have no idea why we need all bumps. This account is actual data holder
+#[account]
+#[derive(Default)]
+pub struct Vault {
+    owner: Pubkey, // 32
+    auth_bump: u8, // 1
+    vault_bump: u8, // 1
+    score: u8, // 1 
+}
+
+impl Vault {
+    const VAULT_DATA_SIZE: usize = 8 + 32 + 1 + 1 + 1;
+
+}
+// need account derived with auth account which will hold account of vault in it.
+#[derive(Accounts)]
+pub struct VaultHolder<'info> {
+    #[account(mut, seeds = [b"vault"], bump = vault.vault_bump)]
+    pub vault: Account<'info, Vault>,
+}
--- cluster2/wba-vault/tests/wba-vault.ts
+++ cluster2/wba-vault/tests/wba-vault.ts
@@ -1,16 +1,85 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { WbaVault } from "../target/types/wba_vault";
+import {IDL,  WbaVault } from "../target/types/wba_vault";
+import { clusterApiUrl, Connection, ConfirmOptions, Commitment, Keypair, PublicKey } from '@solana/web3.js';
+import Fs from '@supercharge/fs';
+import * as os from 'os';
+import path from 'path';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { expect } from 'chai';
+
+chai.use(chaiAsPromised);
+
+async function getAnchorProvider(file:string/*, cluster: any*/): Promise<anchor.AnchorProvider> {
+  const home = os.homedir();
+  const commitment: Commitment = 'confirmed';
+  const connection: Connection = new Connection("http://localhost:8899",/*clusterApiUrl(cluster),*/ commitment);
+  const walletJson = await Fs.readJson(path.join(home, file));
+  const key  = Keypair.fromSecretKey(new Uint8Array(walletJson));
+  const configOpt: ConfirmOptions = {
+    skipPreflight: true,
+    commitment: commitment,
+  }
+  const wallet = new anchor.Wallet(key);
+  return new anchor.AnchorProvider(connection, wallet, configOpt);
+}
 
 describe("wba-vault", () => {
-  // Configure the client to use the local cluster.
+  // if remove this line test will fail!!! Don't know why?
   anchor.setProvider(anchor.AnchorProvider.env());
+  let program = null;
+  let programProvider = null;
+  beforeEach(async function(): Promise<void> {
+        const provider = await getAnchorProvider(".config/solana/id.json"/*, "devnet"*/);
+        anchor.setProvider(provider);
+        program = new Program(IDL, new PublicKey("HAXGA1FwMfL1pgdAeexVTADUbJ6Fk3ziTWKTD9jyjfbt"), programProvider);
+        programProvider = provider;
+    });
 
-  const program = anchor.workspace.WbaVault as Program<WbaVault>;
+  it("init vault", async () => {
+    const owner_keypair = programProvider.wallet;
+const vault_auth_seed = [Buffer.from('auth'), programProvider.wallet.publicKey.toBuffer()] 
+const vault_auth_pubkey = PublicKey.findProgramAddressSync(vault_auth_seed, program.programId)[0];
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+const vault_seed = [Buffer.from('vault'), vault_auth_pubkey.toBuffer()] 
+const vault_state = PublicKey.findProgramAddressSync(vault_seed, program.programId)[0];
+
+    const tx = await program.methods.initialize()
+    .accounts({
+      vaultAuth: vault_auth_pubkey,
+      vaultHolder: vault_state,
+      owner: owner_keypair
+    })
+    .rpc();
+    console.log(`transaction: ${tx}`);
+
+  });
+
+  it("update vault", async () => {
+const vault_auth_seed = [Buffer.from('auth'), programProvider.wallet.publicKey.toBuffer()] 
+const vault_auth_pubkey = PublicKey.findProgramAddressSync(vault_auth_seed, program.programId)[0];
+
+const vault_seed = [Buffer.from('vault'), vault_auth_pubkey.toBuffer()] 
+const vault_state = PublicKey.findProgramAddressSync(vault_seed, program.programId)[0];
+console.log(`provider: ${programProvider.wallet.publicKey}`);
+
+    await program.methods.initialize()
+    .accounts({
+      vaultAuth: vault_auth_pubkey,
+      vaultHolder: vault_state,
+      owner: programProvider.wallet
+    })
+    .rpc();
+
+    const tx = await program.methods.updateScore()
+    .accounts({
+      vault: vault_state,
+    })
+    .signers([programProvider.wallet])
+    .rpc();
     console.log("Your transaction signature", tx);
+    let vault = await program.account.vault.fetch(vault_state);
+    expect(vault.score).to.equal(1);
   });
 });
